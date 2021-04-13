1d0d6023564d7301bd3c66131e80e334e61c087f
Maya Peterson <compoundingsnowballs@gmail.com>
2021-04-12 20:28:55 -0400

    updated FileSystemLoader path and image for loop

--- templates/image_template.py
+++ templates/image_template.py
@@ -1,23 +1,21 @@
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader, Template
-
 #env = Environment(
 #    loader=FileSystemLoader('yourapplication', 'template'),
 #    autoescape=select_autoescape(['html'])
 #)
-
 import jinja2
+from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader, Template
 
-#templateLoader = jinja2.FileSystemLoader(searchpath="./")
-templateLoader = jinja2.FileSystemLoader(searchpath="/Users/mpeterson21/PycharmProjects/whoi-plankton2/templates/")
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
 
 
+autoescape=select_autoescape(['html'])
 templateEnv = jinja2.Environment(loader=templateLoader)
 file = "child_template.html"
 template = templateEnv.get_template(file)
 
 
 
-plankton_images = template.render(images = [
+plankton_images = template.render( images = [
     {'src': 'docs/images/amoeba/IFCB1_2013_302_163814_00313.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton'},
     {'src': 'docs/images/amoeba/IFCB1_2013_302_163814_00313.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton'},
     {'src': 'docs/images/amoeba/IFCB1_2013_302_163814_00313.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton'},
