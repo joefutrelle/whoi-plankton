ffd3ff592a62939894a1881a3ac1eb87fdb75b87
Joe Futrelle <joefutrelle@gmail.com>
2021-04-15 12:34:11 -0400

    3 columns, ragged last column

--- image_template.py
+++ image_template.py
@@ -3,7 +3,8 @@
 #    autoescape=select_autoescape(['html'])
 #)
 import jinja2
-from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoader, Template
+from jinja2 import select_autoescape
+from more_itertools import grouper
 
 templateLoader = jinja2.FileSystemLoader(searchpath="templates")
 
@@ -14,14 +15,17 @@ file = "child_template.html"
 template = templateEnv.get_template(file)
 
 
-plankton_images = template.render( images = [
+images = [
     {'src': 'images/amoeba/IFCB1_2013_302_163814_00313.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'amoeba'},
     {'src': 'images/Apedinella/IFCB5_2017_036_121145_03566.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Apedinella'},
     {'src': 'images/Calciopappus/IFCB1_2009_262_003744_06032.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Calciopappus'},
     {'src': 'images/Chaetoceros_pennate/IFCB1_2010_078_185738_02724.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Chaetoceros_pennate'},
     {'src': 'images/Corethron hystrix/IFCB5_2011_001_165030_05039.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Corethron hystrix'},
-    {'src': 'images/Euglena/IFCB1_2008_318_184203_02927.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Euglena'}
-])
+]
+
+chunks = list(grouper(images, 3))
+
+plankton_images = template.render(rows=chunks)
 
 output_file = 'docs/output.html'
 
