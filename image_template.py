ea2ec80a349f7786403ceb2b543b31236adb39c6
Maya Peterson <compoundingsnowballs@gmail.com>
2021-04-15 15:00:02 -0400

    added extra test images

--- image_template.py
+++ image_template.py
@@ -21,6 +21,17 @@ images = [
     {'src': 'images/Calciopappus/IFCB1_2009_262_003744_06032.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Calciopappus'},
     {'src': 'images/Chaetoceros_pennate/IFCB1_2010_078_185738_02724.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Chaetoceros_pennate'},
     {'src': 'images/Corethron hystrix/IFCB5_2011_001_165030_05039.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Corethron hystrix'},
+{'src': 'images/amoeba/IFCB1_2013_302_163814_00313.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'amoeba'},
+    {'src': 'images/Apedinella/IFCB5_2017_036_121145_03566.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Apedinella'},
+    {'src': 'images/Calciopappus/IFCB1_2009_262_003744_06032.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Calciopappus'},
+    {'src': 'images/Chaetoceros_pennate/IFCB1_2010_078_185738_02724.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Chaetoceros_pennate'},
+    {'src': 'images/Corethron hystrix/IFCB5_2011_001_165030_05039.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Corethron hystrix'},
+{'src': 'images/amoeba/IFCB1_2013_302_163814_00313.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'amoeba'},
+    {'src': 'images/Apedinella/IFCB5_2017_036_121145_03566.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Apedinella'},
+    {'src': 'images/Calciopappus/IFCB1_2009_262_003744_06032.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Calciopappus'},
+    {'src': 'images/Chaetoceros_pennate/IFCB1_2010_078_185738_02724.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Chaetoceros_pennate'},
+    {'src': 'images/Corethron hystrix/IFCB5_2011_001_165030_05039.jpg', 'href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Corethron hystrix'},
+    {'src': 'images/Chaetoceros_pennate/IFCB1_2010_078_185738_02724.jpg','href': 'https://github.com/joefutrelle/whoi-plankton', 'title': 'Chaetoceros_pennate'}
 ]
 
 chunks = list(grouper(images, 3))
